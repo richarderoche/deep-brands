9b5bec273f5d9899311cab01297d85c383bb454a
Richard <richard@infinite-productivity.com>
2026-06-02 12:27:25 -0700

    init

---
+++ studio/schemaTypes/singletons/home.ts
@@ -0,0 +1,34 @@
+import { HomeIcon, StackCompactIcon } from '@sanity/icons'
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'home',
+  title: 'Home',
+  type: 'document',
+  icon: HomeIcon,
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'pbSections',
+      title: 'Page Builder',
+      type: 'pbSections',
+      icon: StackCompactIcon,
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'title',
+    },
+    prepare({ title }) {
+      return {
+        subtitle: 'Home',
+        title,
+      }
+    },
+  },
+})
